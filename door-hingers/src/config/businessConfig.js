aee97a4ac563c01ef4e60199e5c73fade2817ee8
Vinayak M <vinayakmdaz@gmail.com>
2026-09-01 00:35:46 +0530

    second tim

--- door-hingers/src/config/businessConfig.js
+++ door-hingers/src/config/businessConfig.js
@@ -13,7 +13,7 @@ export const businessConfig = {
 
   phone: "+91 9995474127", // digits only with country code, e.g. "+919000000000"
   whatsapp: "9995474127", // digits only with country code, e.g. "919000000000"
-  email: "vinayakmdaz@gmail.com", // e.g. "info@accessoriesland.in"
+  email: "afsupgdi@gmil.com", // ey.g. "info@accessoriesland.i n"
 
   address: "Parappanagadi Road, Kariparambu, Malappuram, Kerala - 676306",
   gstin: "32CBTPA9964R1ZN",
